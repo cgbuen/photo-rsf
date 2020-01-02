50bddb1070bc6619eff88b92732d2f6c76e89f32
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2020-01-02 01:03:30 -0800

    Clean up
    - Update apple-touch-icon to 180x180 version
    - Fix handler names

--- src/about/about-handler.js
+++ src/about/about-handler.js
@@ -1,7 +1,7 @@
 import globalState from '../globalState'
 import { withGlobalState } from 'react-storefront/router'
 
-export default function homeHandler(params, request, response) {
+export default function aboutHandler(params, request, response) {
   return withGlobalState(request, globalState, {
     title: `About ${globalState().title}`,
   })
--- src/inquiries/inquiries-handler.js
+++ src/inquiries/inquiries-handler.js
@@ -1,7 +1,7 @@
 import globalState from '../globalState'
 import { withGlobalState } from 'react-storefront/router'
 
-export default function homeHandler(params, request, response) {
+export default function inquiriesHandler(params, request, response) {
   return withGlobalState(request, globalState, {
     title: `Inquiries ${globalState().title}`,
   })
