f3e41469f4661c8c4778cbb980bf3d22189bd2a1
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2020-01-06 21:39:58 -0800

    Move photo data into s3 bucket and fetch here

--- src/home/home-handler.js
+++ src/home/home-handler.js
@@ -3,9 +3,9 @@ import { withGlobalState } from 'react-storefront/router'
 import photos from './photos'
 import Config from 'react-storefront/Config'
 
-export default function homeHandler(params, request, response) {
+export default async function homeHandler(params, request, response) {
   return withGlobalState(request, globalState, {
     title: `Home ${globalState().title}`,
-    photos: photos(Config.get('assetHost'))
+    photos: await photos(Config.get('assetHost'))
   })
 }
