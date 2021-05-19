c21bf784a40f0d067f48b7a7ad0494a9aece8803
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2021-05-19 03:51:32 -0700

    Update misc list
    - Improve styles
    - Update LinkBlank component to be able to take in className

--- src/components/LinkBlank.js
+++ src/components/LinkBlank.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 import { inject, observer } from 'mobx-react'
+import classnames from 'classnames'
 
 @withStyles(
   theme => ({
@@ -13,9 +14,9 @@ import { inject, observer } from 'mobx-react'
 @observer
 export default class LinkBlank extends Component {
   render() {
-    const { classes, to, children } = this.props
+    const { classes, className, to, children } = this.props
     return (
-      <a className={classes.link} href={to} target="_blank" rel="noopener noreferrer">{children}</a>
+      <a className={classnames(classes.link, className)} href={to} target="_blank" rel="noopener noreferrer">{children}</a>
     )
   }
 }
--- src/misc/Misc.js
+++ src/misc/Misc.js
@@ -4,26 +4,55 @@ import Container from 'react-storefront/Container'
 import Row from 'react-storefront/Row'
 import Typography from '@material-ui/core/Typography'
 import withAmp from 'react-storefront-extensions/amp/withAmp'
+import withStyles from '@material-ui/core/styles/withStyles'
 import LinkBlank from '../components/LinkBlank'
 
+@withStyles(
+  theme => ({
+    list: {
+      padding: 0,
+      '& > li': {
+        marginBottom: 15,
+      },
+    },
+    link: {
+      fontSize: 20,
+    },
+  })
+)
 @withAmp
 @inject(({ app }) => ({ app, social: app.social }))
 @observer
 export default class Misc extends Component {
   render() {
-    const { social } = this.props
+    const { social, classes } = this.props
     return (
       <Container>
         <Row>
           <Typography variant="h1">Miscellaneous</Typography>
         </Row>
         <p>Links to other resources that don't fall under any of the other categories on this site. Some are not mine, but are just added here for my own personal reference.</p>
-        <ul>
-          <li><LinkBlank to="https://forms.gle/tefHXyEh9WsAJjBs9">Film development/scanning signups</LinkBlank>: A service I provide to develop and scan your photo film.</li>
-          <li><LinkBlank to={social.sffb}>Donation page to the San Francisco-Marin Food Bank</LinkBlank>: I send all livestream donations (bits / subscriptions) to the SF-Marin Food Bank, but you can also donate here directly.</li>
-          <li><LinkBlank to="https://ph-1080.cgbuen.com/mckibbin-son-monograph-series.pdf">The McKibbin & Son Monograph Series</LinkBlank>: a paper I wrote in 2011 for a typographic history class about a lesser-known, eight-volume monograph series on publishing and typography in the late 1940s.</li>
-          <li><LinkBlank to="https://matrixzj.github.io/">Keyset archive</LinkBlank> by <LinkBlank to="https://github.com/matrixzj">matrixzj</LinkBlank></li>
-          <li><LinkBlank to="https://drive.google.com/drive/u/0/folders/0B3prVTgFFXASZm1EQkdFbk1JYVk?ths=true">Splatoon 2 map callout guide</LinkBlank> by <LinkBlank to="https://twitter.com/im_flc">Imperious flc</LinkBlank></li>
+        <ul className={classes.list}>
+          <li>
+            <LinkBlank className={classes.link} to="https://forms.gle/tefHXyEh9WsAJjBs9">Film development/scanning signups</LinkBlank>
+            <div>A service I provide to develop and scan your photo film.</div>
+          </li>
+          <li>
+            <LinkBlank className={classes.link} to={social.sffb}>Donation page to the San Francisco-Marin Food Bank</LinkBlank>
+            <div>I send all livestream donations (bits / subscriptions) to the SF-Marin Food Bank, but you can also donate here directly.</div>
+          </li>
+          <li>
+            <LinkBlank className={classes.link} to="https://ph-1080.cgbuen.com/mckibbin-son-monograph-series.pdf">The McKibbin & Son Monograph Series</LinkBlank>
+            <div>A paper I wrote in 2011 for a typographic history class about a lesser-known, eight-volume monograph series on publishing and typography in the late 1940s.</div>
+          </li>
+          <li>
+            <LinkBlank className={classes.link} to="https://matrixzj.github.io/">Keyset archive</LinkBlank>
+            <div>By <LinkBlank to="https://github.com/matrixzj">matrixzj</LinkBlank>. Not by me.</div>
+          </li>
+          <li>
+            <LinkBlank className={classes.link} to="https://drive.google.com/drive/u/0/folders/0B3prVTgFFXASZm1EQkdFbk1JYVk?ths=true">Splatoon 2 map callout guide</LinkBlank>
+            <div>By <LinkBlank to="https://twitter.com/im_flc">Imperious flc</LinkBlank>. Not by me.</div>
+          </li>
         </ul>
       </Container>
     )
