f6c1b49fc3cadcc435aa9751cbaecafd2827a611
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2021-05-20 13:22:58 -0700

    Update to naked .io settings

--- src/globalState.js
+++ src/globalState.js
@@ -46,7 +46,7 @@ export default () => {
     config: Config.values,
     social: {
       instagram: 'https://www.instagram.com/cgbuen/',
-      email: 'chris@cgbuen.com',
+      email: 'chris@cgbuen.io',
       linkedin: 'https://www.linkedin.com/in/christopherbuenaventura/',
       twitch: 'https://www.twitch.tv/cgbuen',
       twitter: 'https://twitter.com/cgbuen',
