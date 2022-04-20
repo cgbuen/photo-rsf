b3436b8933108c9d119d6d4ecff98cd18790835d
Christopher Buenaventura <christopherbuenaventura@gmail.com>
2022-04-19 22:18:19 -0700

    Add/enable active field for gear

--- src/resources/ItemModel.js
+++ src/resources/ItemModel.js
@@ -2,6 +2,7 @@ import { types } from 'mobx-state-tree'
 
 const ItemModel = types.model('ItemModel', {
   id: types.maybeNull(types.number),
+  active: types.maybeNull(types.number),
   name: types.maybeNull(types.string),
   kind: types.maybeNull(types.string),
   url: types.maybeNull(types.string),
